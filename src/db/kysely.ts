9992e1e4c23678c9cf56418c931b2b7bdc3ac3b3
t1enne <nasir.taov@gmail.com>
2025-07-24 00:37:48 +0200

    fix: handle db close

--- src/db/kysely.ts
+++ src/db/kysely.ts
@@ -6,13 +6,25 @@ import { BunSqliteDialect } from "kysely-bun-sqlite";
 const DEVELOPMENT = process.env.NODE_ENV !== "production";
 const dbUrl = DEVELOPMENT ? "./db.sqlite3" : "/data/db.sqlite3";
 console.log("dburl", dbUrl);
-
+const _db = new Database(dbUrl);
 export const db = new Kysely<DatabaseSchema>({
   dialect: new BunSqliteDialect({
-    database: new Database(dbUrl),
+    database: _db,
     onCreateConnection: async (c) => {
       await c.executeQuery(CompiledQuery.raw("PRAGMA journal_mode=WAL"));
     },
   }),
   plugins: [new CamelCasePlugin()],
+});
+
+process.on("SIGTERM", () => {
+  console.log("Received SIGTERM, closing database...");
+  _db.close();
+  process.exit(0);
+});
+
+process.on("SIGINT", () => {
+  console.log("Received SIGINT, closing database...");
+  _db.close();
+  process.exit(0);
 });
